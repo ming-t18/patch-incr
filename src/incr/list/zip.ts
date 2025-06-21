840449748c1cfb000dfea03cb4fe315a2c274f58
Ming <test@example.com>
2025-06-21 00:28:13 -0700

    fix(list): Better zip patches

--- src/incr/list/zip.ts
+++ src/incr/list/zip.ts
@@ -1,16 +1,19 @@
+import { getReplaceOnly, isReplaceOnly } from "../../algebra/replaceOnly";
 import * as ps from "../../patchSchema";
 import { IndexEnd } from "../../patchSchema/types";
-import { CannotReduce, PatchOp, reducePatches } from "../patch";
+import { type PatchEntry, type Patches, PatchOp } from "../patch";
 import type { IF } from "../types";
+import { splice } from "./arrayPatchHelpers";
+import { getMinUpdatedIndex } from "./forwardList";
 
 export const zip = <A, B>(): IF<[A[], B[]], [A, B][]> => {
 	const leftSchema = ps.atomic<A>();
 	const rightSchema = ps.atomic<B>();
 	const leftsSchema = ps.array(leftSchema);
 	const rightsSchema = ps.array(rightSchema);
-	const _inputSchema = ps.tuple(leftsSchema, rightsSchema);
+	const inputSchema = ps.tuple(leftsSchema, rightsSchema);
 	const tupleSchema = ps.tuple(leftSchema, rightSchema);
-	const _outputSchema = ps.array(tupleSchema);
+	const outputSchema = ps.array(tupleSchema);
 	const evaluateZip = ([xs, ys]: [A[], B[]]) => {
 		const zs: [A, B][] = [];
 		const n = xs.length < ys.length ? xs.length : ys.length;
@@ -20,59 +23,103 @@ export const zip = <A, B>(): IF<[A[], B[]], [A, B][]> => {
 		return zs;
 	};
 
-	const forwardZip = reducePatches(evaluateZip, ([xs, ys], entry, _zs) => {
-		const { path, op } = entry;
-		if (path.length === 0 || path.length === 1) {
-			// replace-root or replace entire single array
-			return CannotReduce;
+	const forwardZip = (
+		pair: [A[], B[]],
+		patches: Patches<[A[], B[]]>,
+		_zs: [A, B][],
+	): Patches<[A, B][]> => {
+		const outLen = Math.min(pair[0].length, pair[1].length);
+		const res = inputSchema.analyze(patches);
+		if (res === null) {
+			return outputSchema.empty;
 		}
 
-		const [side, index0, ...rest] = path;
-		if (typeof side !== "number") {
-			return CannotReduce;
+		if (isReplaceOnly(res)) {
+			return outputSchema.fromReplace(evaluateZip(getReplaceOnly(res)));
 		}
 
-		if (typeof index0 !== "number" && index0 !== IndexEnd) {
-			return CannotReduce;
+		if (!res[0] && !res[1]) {
+			return outputSchema.empty;
 		}
 
-		let index: number;
-		const sideLength = side === 0 ? xs.length : ys.length;
-		const minLength = xs.length < ys.length ? xs.length : ys.length;
-		if (index0 === IndexEnd) {
-			index = sideLength;
-		} else {
-			index = index0;
-		}
-
-		if (path.length === 2) {
-			if (op === PatchOp.Replace) {
-				if (index >= minLength) {
-					return CannotReduce;
-				}
-				return [
-					{
-						op: PatchOp.Replace,
-						path: [index, side],
-						value: entry.value,
-					},
-				];
+		const maybeReplaceOnly = (): Patches<[A, B][]> | null => {
+			const dLefts = leftsSchema.analyze(res[0]?.inner ?? leftsSchema.empty);
+			const dRights = rightsSchema.analyze(res[1]?.inner ?? rightsSchema.empty);
+			if (dLefts === null && dRights === null) {
+				return outputSchema.empty;
 			}
 
-			// TODO handle bumping over elements
-			return CannotReduce;
+			if (isReplaceOnly(dLefts)) {
+				return null;
+			}
+			if (isReplaceOnly(dRights)) {
+				return null;
+			}
+
+			const builder = outputSchema.builder();
+			for (const slot of [0, 1] as (0 | 1)[]) {
+				const entrySchema = slot === 0 ? leftSchema : rightSchema;
+				for (const entry of (slot === 0 ? dLefts : dRights) ?? []) {
+					if (entry.path[0] === IndexEnd || entry.path[0] >= outLen) {
+						return null;
+					}
+
+					if ("inner" in entry) {
+						const index = entry.path[0];
+						builder.append(
+							outputSchema.liftIndex(
+								index,
+								tupleSchema.liftIndex(
+									slot,
+									entrySchema.fromPatchEntries([
+										entry.inner as PatchEntry<never>,
+									]),
+								),
+							),
+						);
+					} else {
+						if (entry.op !== PatchOp.Replace) {
+							return null;
+						}
+						const index = entry.path[0];
+						builder.append(
+							outputSchema.liftIndex(
+								index,
+								tupleSchema.liftIndex(
+									slot,
+									entrySchema.fromReplace(entry.value as A & B),
+								),
+							),
+						);
+					}
+				}
+			}
+
+			return builder.build();
+		};
+
+		// TODO find index dividing between repalce-only and bumping over,
+		// and merge both portions
+
+		const res1 = maybeReplaceOnly();
+		if (res1 !== null) {
+			return res1;
 		}
 
-		if (index >= minLength) {
-			return CannotReduce;
-		}
-		return [
-			{
-				...entry,
-				path: [index, side, ...rest],
-			},
-		];
-	});
+		const [xs, ys] = pair;
+		const iMinLeft = getMinUpdatedIndex(xs, res[0]?.inner ?? []);
+		const iMinRight = getMinUpdatedIndex(ys, res[1]?.inner ?? []);
+		const iStart = Math.min(iMinLeft, iMinRight);
+
+		const pair1 = inputSchema.apply(pair, patches);
+		const replacement: [A, B][] = evaluateZip([
+			pair1[0].slice(iStart),
+			pair1[1].slice(iStart),
+		]);
+		return outputSchema.fromPatchEntries(
+			splice(iStart, outLen - iStart, replacement),
+		);
+	};
 
 	return {
 		evaluate: evaluateZip,
