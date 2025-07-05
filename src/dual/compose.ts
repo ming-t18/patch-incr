3411c51a6a51198d8dcf43eae1c090594ffe41d6
Ming <test@example.com>
2025-07-05 00:07:46 -0700

    refactor: Remoe unused code in dual module

--- src/dual/compose.ts
+++
@@ -1,9 +0,0 @@
-import type { DF, DP } from "./types";
-
-export const compose =
-	<X, Y, Z, DX, DY, DZ>(
-		f1: DF<X, Y, DX, DY>,
-		f2: DF<Y, Z, DY, DZ>,
-	): DF<X, Z, DX, DZ> =>
-	(xdx: DP<X, DX>): DP<Z, DZ> =>
-		f2(f1(xdx));
