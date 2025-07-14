5044ed096f05b55b086707ee727963795507754c
Ming <test@example.com>
2025-07-13 17:24:38 -0700

    fix: delete groupBy

--- src/incr/array/groupBy.ts
+++
@@ -1,9 +0,0 @@
-import type { IF } from "../types";
-
-export const groupBy = <Input, Key extends string = string>(
-	getKey: (input: Input) => Key,
-): IF<Input[], Record<Key, Input[]>> => {
-	const _evaluateGroupBuy = (xs: Input[]) =>
-		Object.groupBy(xs, (x) => getKey(x));
-	throw new Error("TODO");
-};
