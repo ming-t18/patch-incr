ebe53311d4df4c8a79ca8ca493be4be58e71834d
Ming <146130+ming-t18@users.noreply.github.com>
2025-12-24 12:02:37 -0800

    feat(cond): switchCase

--- packages/patch-incr/builder/cond.ts
+++ packages/patch-incr/builder/cond.ts
@@ -1,5 +1,7 @@
 import { applyPatches, liftPatch, type Patches, replacePatch } from "../patch";
-import type { IF } from "../types";
+import * as ps from "../patchSchema";
+import type { PatchSchema } from "../patchSchema/types";
+import type { HasForwardOutput, IF } from "../types";
 
 export type CondOutput<A, B> = [true, A] | [false, B];
 
@@ -37,5 +39,49 @@ export const cond = <Input, A, B, DInput = Patches<Input>>(
 						B
 					>);
 		},
+	};
+};
+
+export const switchCase = <
+	Case,
+	Input,
+	Output,
+	InputChange = Patches<Input>,
+	OutputChange = Patches<Output>,
+	ForwardOutput extends boolean = HasForwardOutput,
+>(
+	getCase: (input: Input) => Case,
+	getIF: (
+		case_: Case,
+	) => IF<Input, Output, InputChange, OutputChange, ForwardOutput>,
+	memo = new Map<
+		Case,
+		IF<Input, Output, InputChange, OutputChange, ForwardOutput>
+	>(),
+	inputSchema: PatchSchema<Input, InputChange> = ps.atomic() as never,
+	outputSchema: PatchSchema<Output, OutputChange> = ps.atomic() as never,
+): IF<Input, Output, InputChange, OutputChange, ForwardOutput> => {
+	const ensure = (case_: Case) => {
+		let res = memo.get(case_);
+		if (!res) {
+			res = getIF(case_);
+			memo.set(case_, res);
+		}
+		return res;
+	};
+	const evaluate = (input: Input): Output =>
+		ensure(getCase(input)).evaluate(input);
+	const forward = (input: Input, dx: InputChange, dy?: never): OutputChange => {
+		const case0 = getCase(input);
+		const input1 = inputSchema.apply(input, dx);
+		const case1 = getCase(input1);
+		if (case0 === case1) {
+			return ensure(case0).forward(input, dx, dy as never);
+		}
+		return outputSchema.fromReplace(ensure(case1).evaluate(input1));
+	};
+	return {
+		evaluate,
+		forward: forward as never,
 	};
 };
