454a94e8fb328d79604c3e24611946cd5a809e93
Ming <146130+ming-t18@users.noreply.github.com>
2025-12-27 14:48:30 -0800

    test: Template tests

---
+++ packages/patch-incr/test/template.test.ts
@@ -0,0 +1,63 @@
+import fc from "fast-check";
+import { constant } from "../builder";
+import { map } from "../builder/array";
+import { composer } from "../builder/compose";
+import { accessFor, template } from "../builder/struct";
+import * as gp from "./helpers/genPatched.test";
+import { propsForIF } from "./helpers/props.test";
+
+describe("template", () => {
+	it("should accept constant template", () => {
+		expect(template({}, () => ({ x: 1 })).evaluate(0 as never)).toEqual({
+			x: 1,
+		});
+	});
+
+	it("should throw on unused variables", () => {
+		expect(() =>
+			template({ x: constant(5), unused: constant(0) }, ({ x }) => ({ x1: x })),
+		).toThrow();
+	});
+
+	const genInput = gp.record({
+		a: gp.integer(),
+		b: gp.array(gp.string()),
+		c: gp.string(),
+		d: gp.array(gp.record({ value: gp.string() })),
+	});
+	type Input = gp.InferArbValue<typeof genInput>;
+	const _I = accessFor<Input>();
+
+	const getFunc = () =>
+		template(
+			{
+				x: _I("a"),
+				y: _I("b"),
+				z: composer(_I("d"))
+					.pipe(map(accessFor<{ value: string }>()("value")))
+					.build(),
+			},
+			({ x, y, z }) => ({
+				x,
+				y,
+				z,
+				test: { x, y },
+			}),
+		);
+
+	describe("example function", () => {
+		propsForIF(it, genInput, getFunc);
+
+		it("no replace root", () => {
+			const func = getFunc();
+			fc.assert(
+				fc.property(genInput.arb(), ({ value: x, patches: dx }) => {
+					fc.pre(dx.length > 0 && dx.every((p) => p.path.length > 0));
+					const y = func.evaluate(x);
+					const dy = func.forward(x, dx, y);
+					expect(dy.every((p) => p.path.length > 0));
+				}),
+			);
+		});
+	});
+});
