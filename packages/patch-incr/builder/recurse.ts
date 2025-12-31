4ca8155256960f3b40a7f838d98bd458d634c986
Ming <146130+ming-t18@users.noreply.github.com>
2025-12-31 13:27:38 -0800

    docs: recurse

--- packages/patch-incr/builder/recurse.ts
+++ packages/patch-incr/builder/recurse.ts
@@ -1,8 +1,14 @@
 import type { AnyIF } from "../types";
 
-export const recurse = <F extends AnyIF>(func: (func: F) => F): F => {
+/**
+ * Helper to create a recursive incremental function similar to the Y combinator.
+ * @param getFunc The callback to construct the recursive funtion.
+ * The first argument of the callback is the recursive function itself
+ * in the uninitialized state (empty object).
+ */
+export const recurse = <F extends AnyIF>(getFunc: (rec: F) => F): F => {
 	const rec: F = {} as never;
-	const { evaluate, forward } = func(rec);
+	const { evaluate, forward } = getFunc(rec);
 	rec.evaluate = evaluate;
 	rec.forward = forward;
 	return rec;
