02d13204f0864504f7efddfc05b7e6ed7d4bc4f4
Ming <146130+ming-t18@users.noreply.github.com>
2026-01-02 12:14:35 -0800

    test(dom): Skip if document is not available, replace toBeCalledWith

--- packages/patch-incr-dom/test/patchDOM.test.ts
+++ packages/patch-incr-dom/test/patchDOM.test.ts
@@ -25,265 +25,278 @@ const domPatchCoherent = (
 	expect(actualHTML).toBe(expectedHTML);
 };
 
-describe("changing attributes", () => {
-	it("should change attribute", () => {
-		domPatchCoherent(
-			elem("div", { id: "id1" }),
-			replacePatch("id2", ["attrs", "id"]),
-		);
+const shouldSkip = typeof document !== "object";
+if (shouldSkip) {
+	console.warn("document not found, skipping all DOM tests.");
+}
+
+(shouldSkip ? describe.skip : describe)("patching DOM", () => {
+	describe("changing attributes", () => {
+		it("should change attribute", () => {
+			domPatchCoherent(
+				elem("div", { id: "id1" }),
+				replacePatch("id2", ["attrs", "id"]),
+			);
+		});
+
+		it("should add attribute with Replace patch", () => {
+			domPatchCoherent(
+				elem("div", { id: "id1" }),
+				replacePatch("cls1", ["attrs", "class"]),
+			);
+		});
+
+		it("should add attribute with Add patch", () => {
+			domPatchCoherent(
+				elem("div", { id: "id1" }),
+				addPatch("cls1", ["attrs", "class"]),
+			);
+		});
+
+		it("should remove attribute with Remove patch", () => {
+			domPatchCoherent(
+				elem("div", { id: "id1" }),
+				removePatch(["attrs", "id"]),
+			);
+		});
+
+		it("should do nothing when removing absent attribute", () => {
+			domPatchCoherent(elem("div", {}), removePatch(["attrs", "id"]));
+		});
+
+		it("should delete all attributes", () => {
+			domPatchCoherent(
+				elem("div", { id: "id1", class: "cls1 cls2", style: "color: green;" }),
+				removePatch(["attrs"]),
+			);
+		});
+
+		it("should add all attributes", () => {
+			domPatchCoherent(
+				elem("div"),
+				replacePatch(
+					{
+						id: "id2",
+						"aria-hidden": "true",
+					},
+					["attrs"],
+				),
+			);
+		});
+
+		it("should replace all attributes", () => {
+			domPatchCoherent(
+				elem("div", { id: "id1", class: "cls1 cls2" }),
+				replacePatch(
+					{
+						id: "id2",
+						"aria-hidden": "true",
+					},
+					["attrs"],
+				),
+			);
+		});
 	});
 
-	it("should add attribute with Replace patch", () => {
-		domPatchCoherent(
-			elem("div", { id: "id1" }),
-			replacePatch("cls1", ["attrs", "class"]),
-		);
-	});
+	describe("changing events", () => {
+		let root: HTMLDivElement;
+		let onClick1: jest.Mock<void, []>;
+		let onClick2: jest.Mock<void, []>;
+		let domc: ElementConstruction;
 
-	it("should add attribute with Add patch", () => {
-		domPatchCoherent(
-			elem("div", { id: "id1" }),
-			addPatch("cls1", ["attrs", "class"]),
-		);
-	});
+		beforeEach(() => {
+			onClick1 = jest.fn();
+			onClick2 = jest.fn();
 
-	it("should remove attribute with Remove patch", () => {
-		domPatchCoherent(elem("div", { id: "id1" }), removePatch(["attrs", "id"]));
-	});
-
-	it("should do nothing when removing absent attribute", () => {
-		domPatchCoherent(elem("div", {}), removePatch(["attrs", "id"]));
-	});
-
-	it("should delete all attributes", () => {
-		domPatchCoherent(
-			elem("div", { id: "id1", class: "cls1 cls2", style: "color: green;" }),
-			removePatch(["attrs"]),
-		);
-	});
-
-	it("should add all attributes", () => {
-		domPatchCoherent(
-			elem("div"),
-			replacePatch(
+			root = document.createElement("div");
+			domc = elemEvents(
+				"div",
+				{},
 				{
-					id: "id2",
-					"aria-hidden": "true",
+					click: onClick1,
 				},
-				["attrs"],
-			),
-		);
+			);
+			renderDOM(root, domc);
+		});
+
+		it("should add new event handler", () => {
+			const onKeyDown1 = jest.fn();
+			root.firstChild?.dispatchEvent(new Event("keydown"));
+			expect(onClick1).toHaveBeenCalledTimes(0);
+			expect(onKeyDown1).toHaveBeenCalledTimes(0);
+
+			patchDOM(root, domc, addPatch(onKeyDown1, ["events", "keydown"]));
+			root.firstChild?.dispatchEvent(new Event("keydown"));
+			expect(onClick1).toHaveBeenCalledTimes(0);
+			expect(onKeyDown1).toHaveBeenCalledTimes(1);
+
+			root.firstChild?.dispatchEvent(new Event("click"));
+			expect(onClick1).toHaveBeenCalledTimes(1);
+			expect(onKeyDown1).toHaveBeenCalledTimes(1);
+		});
+
+		it("should replace event handler", () => {
+			root.firstChild?.dispatchEvent(new Event("click"));
+			expect(onClick1).toHaveBeenCalledTimes(1);
+			expect(onClick2).toHaveBeenCalledTimes(0);
+
+			patchDOM(root, domc, replacePatch(onClick2, ["events", "click"]));
+			root.firstChild?.dispatchEvent(new Event("click"));
+			expect(onClick1).toHaveBeenCalledTimes(1);
+			expect(onClick2).toHaveBeenCalledTimes(1);
+		});
+
+		it("should remove event handler", () => {
+			root.firstChild?.dispatchEvent(new Event("click"));
+			expect(onClick1).toHaveBeenCalledTimes(1);
+			expect(onClick2).toHaveBeenCalledTimes(0);
+
+			patchDOM(root, domc, removePatch(["events", "click"]));
+			root.firstChild?.dispatchEvent(new Event("click"));
+			expect(onClick1).toHaveBeenCalledTimes(1);
+			expect(onClick2).toHaveBeenCalledTimes(0);
+		});
 	});
 
-	it("should replace all attributes", () => {
-		domPatchCoherent(
-			elem("div", { id: "id1", class: "cls1 cls2" }),
-			replacePatch(
-				{
-					id: "id2",
-					"aria-hidden": "true",
-				},
-				["attrs"],
-			),
-		);
-	});
-});
+	describe("changing children", () => {
+		it("should replace all children", () => {
+			domPatchCoherent(
+				elem("div", { id: "id1" }, ["text1"]),
+				replacePatch([elem0("em", ["updated"])], ["children"]),
+			);
+		});
 
-describe("changing events", () => {
-	let root: HTMLDivElement;
-	let onClick1: jest.Mock<void, []>;
-	let onClick2: jest.Mock<void, []>;
-	let domc: ElementConstruction;
+		it("should remove all children", () => {
+			domPatchCoherent(elem("div", { id: "id1" }), removePatch(["children"]));
+		});
 
-	beforeEach(() => {
-		onClick1 = jest.fn();
-		onClick2 = jest.fn();
+		describe("children is a list of elements", () => {
+			const listElem = elem("ol", { id: "list1" }, [
+				elem0("li", ["child1"]),
+				elem0("li", ["child2"]),
+				elem0("li", ["child3"]),
+			]);
 
-		root = document.createElement("div");
-		domc = elemEvents(
-			"div",
-			{},
+			describe("should insert new child", () => {
+				const newChild = elem0("li", ["newChild"]);
+				it("in the beginning", () => {
+					domPatchCoherent(listElem, addPatch(newChild, ["children", 0]));
+				});
+
+				it("in the middle", () => {
+					domPatchCoherent(listElem, addPatch(newChild, ["children", 1]));
+				});
+
+				it("in the end by index", () => {
+					domPatchCoherent(listElem, addPatch(newChild, ["children", 2]));
+				});
+
+				it(`in the end by ${IndexEnd} path`, () => {
+					domPatchCoherent(
+						listElem,
+						addPatch(newChild, ["children", IndexEnd]),
+					);
+				});
+			});
+
+			describe("should replace child", () => {
+				const replacedChild = elem0("li", ["replacecChild"]);
+				it("in the beginning", () => {
+					domPatchCoherent(
+						listElem,
+						replacePatch(replacedChild, ["children", 0]),
+					);
+				});
+
+				it("in the middle", () => {
+					domPatchCoherent(
+						listElem,
+						replacePatch(replacedChild, ["children", 1]),
+					);
+				});
+
+				it("in the end", () => {
+					domPatchCoherent(
+						listElem,
+						replacePatch(replacedChild, ["children", 2]),
+					);
+				});
+			});
+
+			describe("should remove child", () => {
+				it("in the beginning", () => {
+					domPatchCoherent(listElem, removePatch(["children", 0]));
+				});
+
+				it("in the middle", () => {
+					domPatchCoherent(listElem, removePatch(["children", 1]));
+				});
+
+				it("in the end", () => {
+					domPatchCoherent(listElem, removePatch(["children", 2]));
+				});
+			});
+		});
+
+		const newChilds = [
+			{ label: "element", child: elem0("span", ["test"]) },
+			{ label: "text string", child: "string" },
+			{ label: "empty string", child: "" },
+			{ label: "whitespace string", child: "  " },
+			{ label: "non-string", child: 12n },
+		];
+
+		const parents: { label: string; parent: ElementConstruction }[] = [
 			{
-				click: onClick1,
+				label: "list of text nodes",
+				parent: elem0("p", ["text ", 1, ". "]),
 			},
-		);
-		renderDOM(root, domc);
+			{
+				label: "mix of text nodes and elements",
+				parent: elem0("p", ["text ", 1, ". ", "", elem0("em", ["test"]), 1]),
+			},
+		];
+
+		for (const { label: label0, parent } of parents) {
+			describe(`children is a ${label0}`, () => {
+				for (const { label, child } of newChilds) {
+					describe(`should insert new ${label} child`, () => {
+						it("in the beginning", () => {
+							domPatchCoherent(parent, addPatch(child, ["children", 0]));
+						});
+
+						it("in the middle 1", () => {
+							domPatchCoherent(parent, addPatch(child, ["children", 1]));
+						});
+
+						it("in the middle 2", () => {
+							domPatchCoherent(parent, addPatch(child, ["children", 2]));
+						});
+
+						it("in the end by index", () => {
+							domPatchCoherent(parent, addPatch(child, ["children", 3]));
+						});
+
+						it(`in the end by ${IndexEnd}`, () => {
+							domPatchCoherent(parent, addPatch(child, ["children", IndexEnd]));
+						});
+					});
+
+					describe("should replace existing child", () => {
+						it("in the beginning", () => {
+							domPatchCoherent(parent, replacePatch(child, ["children", 0]));
+						});
+
+						it("in the middle 1", () => {
+							domPatchCoherent(parent, replacePatch(child, ["children", 1]));
+						});
+
+						it("in the middle 2", () => {
+							domPatchCoherent(parent, replacePatch(child, ["children", 2]));
+						});
+					});
+				}
+			});
+		}
 	});
-
-	it("should add new event handler", () => {
-		const onKeyDown1 = jest.fn();
-		root.firstChild?.dispatchEvent(new Event("keydown"));
-		expect(onClick1).toBeCalledTimes(0);
-		expect(onKeyDown1).toBeCalledTimes(0);
-
-		patchDOM(root, domc, addPatch(onKeyDown1, ["events", "keydown"]));
-		root.firstChild?.dispatchEvent(new Event("keydown"));
-		expect(onClick1).toBeCalledTimes(0);
-		expect(onKeyDown1).toBeCalledTimes(1);
-
-		root.firstChild?.dispatchEvent(new Event("click"));
-		expect(onClick1).toBeCalledTimes(1);
-		expect(onKeyDown1).toBeCalledTimes(1);
-	});
-
-	it("should replace event handler", () => {
-		root.firstChild?.dispatchEvent(new Event("click"));
-		expect(onClick1).toBeCalledTimes(1);
-		expect(onClick2).toBeCalledTimes(0);
-
-		patchDOM(root, domc, replacePatch(onClick2, ["events", "click"]));
-		root.firstChild?.dispatchEvent(new Event("click"));
-		expect(onClick1).toBeCalledTimes(1);
-		expect(onClick2).toBeCalledTimes(1);
-	});
-
-	it("should remove event handler", () => {
-		root.firstChild?.dispatchEvent(new Event("click"));
-		expect(onClick1).toBeCalledTimes(1);
-		expect(onClick2).toBeCalledTimes(0);
-
-		patchDOM(root, domc, removePatch(["events", "click"]));
-		root.firstChild?.dispatchEvent(new Event("click"));
-		expect(onClick1).toBeCalledTimes(1);
-		expect(onClick2).toBeCalledTimes(0);
-	});
-});
-
-describe("changing children", () => {
-	it("should replace all children", () => {
-		domPatchCoherent(
-			elem("div", { id: "id1" }, ["text1"]),
-			replacePatch([elem0("em", ["updated"])], ["children"]),
-		);
-	});
-
-	it("should remove all children", () => {
-		domPatchCoherent(elem("div", { id: "id1" }), removePatch(["children"]));
-	});
-
-	describe("children is a list of elements", () => {
-		const listElem = elem("ol", { id: "list1" }, [
-			elem0("li", ["child1"]),
-			elem0("li", ["child2"]),
-			elem0("li", ["child3"]),
-		]);
-
-		describe("should insert new child", () => {
-			const newChild = elem0("li", ["newChild"]);
-			it("in the beginning", () => {
-				domPatchCoherent(listElem, addPatch(newChild, ["children", 0]));
-			});
-
-			it("in the middle", () => {
-				domPatchCoherent(listElem, addPatch(newChild, ["children", 1]));
-			});
-
-			it("in the end by index", () => {
-				domPatchCoherent(listElem, addPatch(newChild, ["children", 2]));
-			});
-
-			it(`in the end by ${IndexEnd} path`, () => {
-				domPatchCoherent(listElem, addPatch(newChild, ["children", IndexEnd]));
-			});
-		});
-
-		describe("should replace child", () => {
-			const replacedChild = elem0("li", ["replacecChild"]);
-			it("in the beginning", () => {
-				domPatchCoherent(
-					listElem,
-					replacePatch(replacedChild, ["children", 0]),
-				);
-			});
-
-			it("in the middle", () => {
-				domPatchCoherent(
-					listElem,
-					replacePatch(replacedChild, ["children", 1]),
-				);
-			});
-
-			it("in the end", () => {
-				domPatchCoherent(
-					listElem,
-					replacePatch(replacedChild, ["children", 2]),
-				);
-			});
-		});
-
-		describe("should remove child", () => {
-			it("in the beginning", () => {
-				domPatchCoherent(listElem, removePatch(["children", 0]));
-			});
-
-			it("in the middle", () => {
-				domPatchCoherent(listElem, removePatch(["children", 1]));
-			});
-
-			it("in the end", () => {
-				domPatchCoherent(listElem, removePatch(["children", 2]));
-			});
-		});
-	});
-
-	const newChilds = [
-		{ label: "element", child: elem0("span", ["test"]) },
-		{ label: "text string", child: "string" },
-		{ label: "empty string", child: "" },
-		{ label: "whitespace string", child: "  " },
-		{ label: "non-string", child: 12n },
-	];
-
-	const parents: { label: string; parent: ElementConstruction }[] = [
-		{
-			label: "list of text nodes",
-			parent: elem0("p", ["text ", 1, ". "]),
-		},
-		{
-			label: "mix of text nodes and elements",
-			parent: elem0("p", ["text ", 1, ". ", "", elem0("em", ["test"]), 1]),
-		},
-	];
-
-	for (const { label: label0, parent } of parents) {
-		describe(`children is a ${label0}`, () => {
-			for (const { label, child } of newChilds) {
-				describe(`should insert new ${label} child`, () => {
-					it("in the beginning", () => {
-						domPatchCoherent(parent, addPatch(child, ["children", 0]));
-					});
-
-					it("in the middle 1", () => {
-						domPatchCoherent(parent, addPatch(child, ["children", 1]));
-					});
-
-					it("in the middle 2", () => {
-						domPatchCoherent(parent, addPatch(child, ["children", 2]));
-					});
-
-					it("in the end by index", () => {
-						domPatchCoherent(parent, addPatch(child, ["children", 3]));
-					});
-
-					it(`in the end by ${IndexEnd}`, () => {
-						domPatchCoherent(parent, addPatch(child, ["children", IndexEnd]));
-					});
-				});
-
-				describe("should replace existing child", () => {
-					it("in the beginning", () => {
-						domPatchCoherent(parent, replacePatch(child, ["children", 0]));
-					});
-
-					it("in the middle 1", () => {
-						domPatchCoherent(parent, replacePatch(child, ["children", 1]));
-					});
-
-					it("in the middle 2", () => {
-						domPatchCoherent(parent, replacePatch(child, ["children", 2]));
-					});
-				});
-			}
-		});
-	}
 });
