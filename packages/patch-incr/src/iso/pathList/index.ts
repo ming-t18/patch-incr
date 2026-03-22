946fb20a375acd3ae92d192dff3bfbba855f7caa
Ming <146130+ming-t18@users.noreply.github.com>
2026-03-22 09:55:10 -0700

    refactor: Delete code for path list optics

--- packages/patch-incr/src/iso/pathList/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./access";
-export * from "./array";
-export * from "./builder";
-export * from "./pair";
--- packages/patch-incr/src/iso/pathList/pair.ts
+++
@@ -1,5 +0,0 @@
-import { accessPath } from "./access";
-import type { PathListOptics } from "./types";
-
-export const fst = <A, B>(): PathListOptics<[A, B], A> => accessPath([0]);
-export const snd = <A, B>(): PathListOptics<[A, B], B> => accessPath([1]);
