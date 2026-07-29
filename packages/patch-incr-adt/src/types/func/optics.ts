bd41d16c3bf8df66d4db0804075ba64d79e9b6d5
Ming <146130+ming-t18@users.noreply.github.com>
2026-07-28 21:02:49 -0700

    refactor(adt): Rename optics

--- packages/patch-incr-adt/src/types/func/optics.ts
+++ packages/patch-incr-adt/src/types/func/optics.ts
@@ -3,28 +3,33 @@ import type { APair } from "@/pair";
 import type { $A } from "../abbr";
 import type { IF, IFA, IFC } from "./incrFunc";
 
-export interface ISet<S extends $A, A extends $A> {
+export interface ISetter<S extends $A, A extends $A> {
 	readonly set: IF<APair<S, A>, S>;
 	readonly modify: (func: IF<A, A>) => IF<S, S>;
 	readonly modifyCtx: <Ctx extends $A>(func: IFC<Ctx, A, A>) => IFC<Ctx, S, S>;
 }
 
-export interface ISet1<S extends $A, T extends $A, A extends $A, B extends $A> {
+export interface ISetter1<
+	S extends $A,
+	T extends $A,
+	A extends $A,
+	B extends $A,
+> {
 	readonly set: IF<APair<S, B>, T>;
 	readonly modify: (func: IF<A, B>) => IF<S, T>;
 	readonly modifyCtx: <Ctx extends $A>(func: IFC<Ctx, A, B>) => IFC<Ctx, S, T>;
 }
 
-export interface ILens<S extends $A, A extends $A> extends ISet<S, A> {
+export interface ILens<S extends $A, A extends $A> extends ISetter<S, A> {
 	readonly get: IFA<S, A>;
 }
 
 export interface ILens1<S extends $A, T extends $A, A extends $A, B extends $A>
-	extends ISet1<S, T, A, B> {
+	extends ISetter1<S, T, A, B> {
 	readonly get: IFA<S, A>;
 }
 
-export interface IAffine<S extends $A, A extends $A> extends ISet<S, A> {
+export interface IAffine<S extends $A, A extends $A> extends ISetter<S, A> {
 	readonly view: IFA<S, AOption<A>>;
 }
 
@@ -33,7 +38,7 @@ export interface IAffine1<
 	T extends $A,
 	A extends $A,
 	B extends $A,
-> extends ISet1<S, T, A, B> {
+> extends ISetter1<S, T, A, B> {
 	readonly view: IFA<S, AOption<A>>;
 }
 
